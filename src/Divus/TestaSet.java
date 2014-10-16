f55b0736318f5d87e41ae0126bcdbd8a47506dbc
Thomaz Stepheson <thomazsouzagomes@gmail.com>
2014-10-15 22:11:36 -0400

    15/10/2014 - Interação 04

--- src/Divus/TestaSet.java
+++ src/Divus/TestaSet.java
@@ -1,11 +1,12 @@
 package Divus;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class TestaSet {
 	public static void main(String[] args) {
-		Set set = new HashSet();
+		Set set = new LinkedHashSet();
 		
 		set.add("Ana");
 		set.add("Wanessa");
